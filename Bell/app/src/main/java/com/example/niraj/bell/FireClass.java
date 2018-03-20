a0c2cbced81b4ace1ec1942324d39445dd467286
Niraj <nirajtiwary98@gmail.com>
2018-03-21 00:09:31 +0530

    commited

---
+++ Bell/app/src/main/java/com/example/niraj/bell/FireClass.java
@@ -0,0 +1,8 @@
+package com.example.niraj.bell;
+
+/**
+ * Created by niraj on 19/3/18.
+ */
+
+public class fireClass {
+}
---
+++ SmartBell/app/src/main/java/MyFirebaseInstanceIdService.java
@@ -0,0 +1,27 @@
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by niraj on 20/3/18.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }
+
+
+}
